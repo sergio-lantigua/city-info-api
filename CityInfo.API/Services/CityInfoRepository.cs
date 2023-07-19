2924b385f89e2000f4c0a59ed2671f3cb392fc78
sergio adrian lantigua de peña <Ing_sergiolantigua@hotmail.com>
2023-07-18 20:37:01 -0400

    add pagination, filter and search

--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -26,6 +26,33 @@ namespace CityInfo.API.Services
             return await _context.Cities.OrderBy(c => c.Name).ToListAsync();
         }
 
+        public async Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(string? name, string? searchQuery, int pageNumber, int pageSize)
+        {
+            //collection from the start
+            var collection = _context.Cities as IQueryable<City>;
+
+            if(!string.IsNullOrEmpty(name)) { 
+            
+                name = name.Trim();
+                collection = collection.Where(c => c.Name == name); 
+            }
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                searchQuery = searchQuery.Trim();
+                collection = collection.Where(c => c.Name.Contains(searchQuery) || (c.Description != null && c.Description.Contains(searchQuery)));
+            }
+
+            var totalAmount = await collection.CountAsync();
+
+            var paginationMetadata = new PaginationMetadata(
+                totalAmount, pageSize, pageNumber );
+
+            var collectionToReturn = await collection.OrderBy(c => c.Name).Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToListAsync();
+
+            return (collectionToReturn, paginationMetadata);
+        }
+
         public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
         {
             if (includePointsOfInterest) { 
